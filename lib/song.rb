137e14c75d29d4cc7b8b49d8ac5d17ded9be3249
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-04 22:42:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   
   attr_accessor :title, :artist
   
-  def initialize(title, artist)
+  def initialize(title = "I shouldn't have to do this", artist = "whatever")
     @title = title
     @artist = artist
   end
